4d97c407ba2bcd36745ab4a1124284bc71a63ea2
norhan699 <nonozxp@gmail.com>
2022-05-29 22:09:17 +0200

    Put property file attributes into HashMap

--- src/main/java/Utils/ReadPropertiesFile.java
+++ src/main/java/Utils/ReadPropertiesFile.java
@@ -2,19 +2,25 @@ package Utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 public final class ReadPropertiesFile {
-
+    static final Map<String,String> CONFIGMAP = new HashMap<>();
     public static String getValue(String key) throws Exception {
-        String value="";
+        //String value="";
         Properties property = new Properties();
         FileInputStream filePath =new FileInputStream(System.getProperty("user.dir")+"/src/main/resources/properties");
         property.load(filePath);
-        value = property.getProperty(key);
-        if(Objects.isNull(value)){//=value==null
+       // value = property.getProperty(key);=da b2a bdalo CONFIGMAP.get(key);
+        for(Map.Entry<Object,Object> entry: property.entrySet()){
+            CONFIGMAP.put(String.valueOf(entry.getKey()),String.valueOf(entry.getValue()));
+        }
+        if(Objects.isNull(CONFIGMAP.get(key))){//=value==null//Objects.isNull(value)
             throw new Exception("property name "+key+" is not found, please check it in resources.properties file");
         }
-        return value;
+        return CONFIGMAP.get(key);
+        //return value;
     }
 }
